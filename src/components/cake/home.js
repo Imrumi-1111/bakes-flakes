cd246f267d00de82481ff592c211ffa719987084
Imrumi-1111 <upasanachakraboty18@gmail.com>
2023-06-25 15:53:50 +0530

    name added

--- src/components/cake/home.js
+++ src/components/cake/home.js
@@ -11,13 +11,13 @@ export default function Home() {
             <div className={styles.Home}>
                 <NavBar />
                 <div className={styles.slide}>
-                    <div className={styles.images}><img src="./images/cake2.jpg" alt="cake" height={300}/></div>
-                    <div className={styles.images}><img src="./images/chocopie.jpg" alt="cake" height={300}/></div>
-                    <div className={styles.images}><img src="./images/au gratin.jpg" alt="true" height={300}/></div>
-                    <div className={styles.images}><img src="./images/choco.jpeg" alt="cake" height={300}/></div>
-                    <div className={styles.images}><img src="./images/cheesy pizza.jpg" alt="pizza" height={300}/></div>
-                    <div className={styles.images}><img src="./images/potato au gratin.jpg" alt="potato au gratin" height={300}/></div>
-                    <div className={styles.images}><img src="./images/plum-cake.jpg" alt="cake" height={300}/></div>
+                    <div className={styles.images}><img src="./images/cake2.jpg" alt="cake" height={300}/><p>Tricoloured cake</p></div>
+                    <div className={styles.images}><img src="./images/chocopie.jpg" alt="cake" height={300}/><p>Chocolate cake</p></div>
+                    <div className={styles.images}><img src="./images/au gratin.jpg" alt="true" height={300}/><p>Au gratin</p></div>
+                    <div className={styles.images}><img src="./images/choco.jpeg" alt="cake" height={300}/><p>Chocolate cake</p></div>
+                    <div className={styles.images}><img src="./images/cheesy pizza.jpg" alt="pizza" height={300}/><p>Pizza</p></div>
+                    <div className={styles.images}><img src="./images/potato au gratin.jpg" alt="potato au gratin" height={300}/><p>Potato au gratin</p></div>
+                    <div className={styles.images}><img src="./images/plum-cake.jpg" alt="cake" height={300}/><p>Plum cake</p></div>
                 </div>
                 <h2> Surprise your loved one</h2>
                 <div className={styles.May}><MayLike/></div>
